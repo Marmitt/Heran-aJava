2949f0523bd394e842354aa791e9ceb424928253
Mateus Marmitt <lmateus.marmitt@gmail.com>
2017-10-03 19:57:21 -0300

    Teste

---
+++ src/br/com/institutoivoti/TesteFuncionario.java
@@ -0,0 +1,17 @@
+package br.com.institutoivoti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteFuncionario {
+
+	@Test
+	public void test() {
+		Patrao patrao = new Patrao();
+		patrao.Senha(9865);
+		assertEquals(gerente.autentica(9865), true);
+
+	}
+
+}
