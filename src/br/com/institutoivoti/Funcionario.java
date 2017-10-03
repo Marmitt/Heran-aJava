d8da71868eea60284702cb5e75a110443cef1f56
Mateus Marmitt <lmateus.marmitt@gmail.com>
2017-10-03 19:51:31 -0300

    Primeiros Commits

---
+++ src/br/com/institutoivoti/Funcionario.java
@@ -0,0 +1,13 @@
+package br.com.institutoivoti;
+
+public class Funcionario {
+
+	double salario;
+	String cpf;
+	
+	public double Getsalario(double salarioFuncionario) {
+		return salario = salarioFuncionario;
+		
+	}
+
+}
---
+++ src/br/com/institutoivoti/Patrao.java
@@ -0,0 +1,10 @@
+package br.com.institutoivoti;
+
+public class Patrao {
+
+	public static void main(String[] args) {
+
+
+	}
+
+}
