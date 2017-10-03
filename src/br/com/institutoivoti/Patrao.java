2e8ae6b622c011b51db09aea27208191bec274cf
Mateus Marmitt <lmateus.marmitt@gmail.com>
2017-10-03 20:26:56 -0300

    Novos commits

--- src/br/com/institutoivoti/Patrao.java
+++ src/br/com/institutoivoti/Patrao.java
@@ -1,17 +1,26 @@
 package br.com.institutoivoti;
 
 public class Patrao extends Funcionario {
-	
+
 	String nome;
 	int senha;
 	int numeroFuncionariosGere;
-	
+
 	public String Getnome(String nomep) {
 		return nome = nomep;
 	}
-	
+
 	public int Senha(int senhap) {
 		return senha = senhap;
 	}
-	
+
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			System.out.println("Acesso Liberado");
+			return true;
+		} else {
+			System.out.println("Acesso Negado");
+			return false;
+		}
+	}
 }
