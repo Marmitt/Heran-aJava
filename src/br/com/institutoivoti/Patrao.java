9585ce363cfe75f4e1a6528d8fc056d1f5757117
Mateus Marmitt <lmateus.marmitt@gmail.com>
2017-10-03 20:00:22 -0300

    Testes

--- src/br/com/institutoivoti/Patrao.java
+++ src/br/com/institutoivoti/Patrao.java
@@ -1,10 +1,17 @@
 package br.com.institutoivoti;
 
-public class Patrao {
-
-	public static void main(String[] args) {
-
-
+public class Patrao extends Funcionario {
+	
+	String nome;
+	int senha;
+	int numeroFuncionariosGere;
+	
+	public String Getnome(String nomep) {
+		return nome = nomep;
 	}
-
+	
+	public int Senha(int senhap) {
+		return senha = senhap;
+	}
+	
 }
